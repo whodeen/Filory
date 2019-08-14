8088718449546119c55b5e249afcbb6037b942cd
Pure <whodeen@gmail.com>
2019-08-14 21:20:43 +0300

    Added registration function

---
+++ WebAPI/Models/AuthenticationContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class AuthenticationContext :IdentityDbContext
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public AuthenticationContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+    }
+}
---
+++ WebAPI/obj/Debug/netcoreapp2.2/bb4zf5gs.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE `__EFMigrationsHistory` 
+( 
+    `MigrationId` nvarchar(150) NOT NULL, 
+    `ProductVersion` nvarchar(32) NOT NULL, 
+     PRIMARY KEY (`MigrationId`) 
+);
+
+  IF EXISTS(SELECT 1 FROM information_schema.tables  WHERE table_name = "__EFMigrationsHistory" AND table_schema = DATABASE() ) 
+BEGIN
+CREATE TABLE `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) NOT NULL,
+    `ProductVersion` varchar(32) NOT NULL,
+    PRIMARY KEY (`MigrationId`)
+);
+
+END;
+
+
+
+CREATE TABLE `ApplicationUser` (
+    `Id` varchar(767) NOT NULL,
+    `UserName` text NULL,
+    `NormalizedUserName` text NULL,
+    `Email` text NULL,
+    `NormalizedEmail` text NULL,
+    `EmailConfirmed` bit NOT NULL,
+    `PasswordHash` text NULL,
+    `SecurityStamp` text NULL,
+    `ConcurrencyStamp` text NULL,
+    `PhoneNumber` text NULL,
+    `PhoneNumberConfirmed` bit NOT NULL,
+    `TwoFactorEnabled` bit NOT NULL,
+    `LockoutEnd` timestamp NULL,
+    `LockoutEnabled` bit NOT NULL,
+    `AccessFailedCount` int NOT NULL,
+    `Login` text NULL,
+    PRIMARY KEY (`Id`)
+);
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20190811220323_InitialCreate', '2.2.2-servicing-10034');
+
